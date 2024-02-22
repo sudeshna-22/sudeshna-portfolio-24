98e945e6cc8124bc38469cc2664c7f3dd7c23f70
sudeshna-22 <pandeysudeshna9@gmail.com>
2024-02-22 09:46:36 +0545

    modified to update

--- src/components/additional/UnderDevelopment.jsx
+++ src/components/additional/UnderDevelopment.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UnderDevelopment = () => {
   return (
-    <div>This site is under development, it will be live soon. Thank You for Visiting!</div>
+    <div>This site is under development, it will be live soon. Thank You for Visiting :) !!!</div>
   )
 }
 
